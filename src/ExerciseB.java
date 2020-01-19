03e9ca33a7fc1fdc8520fdc4e588405fe3295230
Daniel Porto <daniel.porto@gmail.com>
2020-01-19 12:39:10 +0000

    Test3 - wrap the HashMap with Collections.synchronized fix the race condition. However serializes accesses to the map making it very slow. (do not scale) reason: we still need to synchronize on the block because synchronize collections doesn't support tread safe iteration, i.e. serial access. That's equivalent to the synchronize method. the downside compared to concurrent data structures: it locks even in read operations.

--- src/ExerciseB.java
+++ src/ExerciseB.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class ExerciseB {
     public static final int NUM_ITER = 1000;
@@ -17,23 +21,25 @@ public class ExerciseB {
                 //select an element to change
                 int id = numGenerator.nextInt(NUM_ELEMENTS);
 
-                // add/update the element to the database
-                if(database.containsKey(id)){
-                    //update the element
-                    Integer element = database.get(id);
-                    element+=1;
-                    database.put(id, element);
-                }
-                else{
-                    //create the element
-                    database.put(id, 1);
+                synchronized (database) {
+                    // add/update the element to the database
+                    if (database.containsKey(id)) {
+                        //update the element
+                        Integer element = database.get(id);
+                        element += 1;
+                        database.put(id, element);
+                    } else {
+                        //create the element
+                        database.put(id, 1);
+                    }
                 }
             }//for
         }//run
     }//Threadclass
 
     public static void main (String[] args) throws Exception {
-        Map<Integer, Integer> DB = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> DB = Collections.synchronizedMap(new HashMap<Integer, Integer>());
+
         Thread a = new MyThread(DB);
         Thread b = new MyThread(DB);
 
@@ -42,7 +48,6 @@ public class ExerciseB {
 
         a.join();
         b.join();
-
         // sum the elements in the map
         int total = 0;
         for(int i=0; i < NUM_ELEMENTS; i++) {
@@ -53,5 +58,6 @@ public class ExerciseB {
             }
         }//for
         System.out.println("Total items:"+total);
+
     }
 }
